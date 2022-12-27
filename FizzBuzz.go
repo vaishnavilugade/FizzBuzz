fd4ed20fec7d08ed9afc7b590254f464f46452d7
Vaishnavi Lugade <108423518+vaishnavilugade@users.noreply.github.com>
2022-12-27 20:25:13 +0530

    Add files via upload

---
+++ FizzBuzz.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scanln(&n)
+	for i := 1; i <= n; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Print("FizzBuzz\n")
+		} else if i%5 == 0 {
+			fmt.Print("Buzz\n")
+		} else if i%3 == 0 {
+			fmt.Print("Fizz\n")
+		} else {
+			fmt.Print(i, "\n")
+		}
+	}
+}
